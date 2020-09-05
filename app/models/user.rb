2b8fe499ec95b5e514b83dd89163f7b766b35672
Karim Timer <abdikarimtimer@gmail.com>
2020-09-05 18:17:09 +0100

    part 1 create channels and be able to remove them too

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
+  devise :masqueradable, :database_authenticatable,
+         :registerable, :recoverable, :rememberable, :validatable, :omniauthable
 
   has_one_attached :avatar
   has_person_name
 
   has_many :notifications, as: :recipient
   has_many :services
+  has_many :channel_users, dependent: :destroy
+  has_many :channels, through: :channel_users
+  has_many :messages, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :channels do
+    resource :channel_user
+  end
+
+  namespace :admin do
+    resources :users
+    resources :announcements
+    resources :notifications
+    resources :services
+
+    root to: "users#index"
+  end
   get '/privacy', to: 'home#privacy'
   get '/terms', to: 'home#terms'
     authenticate :user, lambda { |u| u.admin? } do
@@ -11,6 +23,6 @@ Rails.application.routes.draw do
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'home#index'
+  root to: 'channels#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
